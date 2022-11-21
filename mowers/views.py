c08ce1eb64f0d79dc215d2f6926f8602a23c62b2
AndrewBonds55 <92964384+AndrewBonds55@users.noreply.github.com>
2022-11-20 19:57:14 -0600

    tests and pages for mowers app

--- mowers/views.py
+++ mowers/views.py
@@ -1,7 +1,17 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse("Hi, we are learning Django and Python")
-    
+#def homePageView(request):
+ #   return HttpResponse("Welcome to Mowers!")
+
+class HomePageView(TemplateView):
+    template_name = "mowers/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "mowers/about.html"
+
+
+
+
